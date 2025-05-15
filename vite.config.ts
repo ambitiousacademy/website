c957ed5ae2abead35cf82e6dc661167cd3c92585
Abhay Mishra <grabhaymishra@gmail.com>
2025-05-16 00:51:15 +0530

    added vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,23 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: '::',
     port: 8080,
+  },
+  build: {
+    outDir: 'dist',
   },
   plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  // 👇 This tells Vite to fallback to index.html for SPA routing
+  base: '/',
+  preview: {
+    port: 5000,
   },
 }));
